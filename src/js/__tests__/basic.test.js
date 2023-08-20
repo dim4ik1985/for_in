520c34758a45681759fb05e654980dacb47df9d0
dim4ik85 <dim4ik020985@gmail.com>
2023-08-20 20:42:16 +0300

    init

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,6 +1,18 @@
-import multiplication from '../basic';
+import orderByProps from '../basic';
 
-test('should multiplication', () => {
-  const result = [2, 2];
-  expect(multiplication(result)).toBe(4);
+test('правильная структура объекта', () => {
+  const personParam = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const sortMethod = ['name', 'level'];
+  const heroes = orderByProps(personParam, sortMethod);
+  const correct = [
+    { key: 'name', value: 'мечник' },
+    { key: 'level', value: 2 },
+    { key: 'attack', value: 80 },
+    { key: 'defence', value: 40 },
+    { key: 'health', value: 10 },
+  ];
+
+  expect(heroes).toEqual(correct);
 });
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,3 +1,20 @@
-export default function multiplication(nums) {
-  return nums.reduce((total, num) => total * num, 1);
+export default function orderByProps(obj, sortingOrder) {
+  const arrSorted = [];
+  Object.entries(obj).sort((a, b) => (a > b ? 1 : -1)).forEach((el) => {
+    if (sortingOrder.indexOf(el[0]) === -1) {
+      arrSorted.push(el[0]);
+    }
+  });
+
+  const arrSortedResult = sortingOrder.concat(arrSorted);
+  const cardHeroes = [];
+
+  // eslint-disable-next-line guard-for-in
+  for (const prop in arrSortedResult) {
+    const copyObj = {};
+    copyObj.key = arrSortedResult[prop];
+    copyObj.value = obj[arrSortedResult[prop]];
+    cardHeroes.push(copyObj);
+  }
+  return cardHeroes;
 }
